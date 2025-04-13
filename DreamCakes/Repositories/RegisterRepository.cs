ca07395dce95089e25c30b8bafa23c7c8fe44c20
LauraOrtiz11 <ortizlaurava@gmail.com>
2025-04-13 16:05:40 -0500

    RF001-Registro ya funcional sin estilos y encriptación de la contraseña. Nueva estrucutra y modelos con entity.

---
+++ DreamCakes/Repositories/RegisterRepository.cs
@@ -0,0 +1,49 @@
+﻿using DreamCakes.Dtos;
+using DreamCakes.Repositories.Models; 
+using System;
+using System.Linq;
+
+namespace DreamCakes.Repositories
+{
+    public class RegisterRepository
+    {
+        private readonly DreamCakesEntities context;
+
+        public RegisterRepository()
+        {
+            context = new DreamCakesEntities();
+        }
+
+        public bool CreateUser(RegisterDto userDto)
+        {
+            try
+            {
+                var user = new USUARIO
+                {
+                    Nombres = userDto.Nombres,
+                    Apellidos = userDto.Apellidos,
+                    Telefono = userDto.Telefono,
+                    Direccion = userDto.Direccion,
+                    Email = userDto.Email,
+                    Contrasena = userDto.Contrasena,   
+                    ID_Estado = userDto.ID_Estado,
+                    ID_Rol = userDto.ID_Rol
+                };
+
+                // Agregar el usuario al DbSet USUARIO
+                context.USUARIOs.Add(user);
+
+                // Guardar los cambios en la base de datos
+                context.SaveChanges();
+
+                return true;  // Registro exitoso
+            }
+            catch (Exception ex)
+            {
+                // Manejo de errores 
+                Console.WriteLine("Error al registrar usuario: " + ex.Message);
+                return false;
+            }
+        }
+    }
+}
---
+++ DreamCakes/Utilities/EncryptUtility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DreamCakes.Utilities
+{
+    public class EncryptUtility
+    {
+        public static string HashPassword(string contrasena)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(contrasena);
+        }
+
+    }
+}
