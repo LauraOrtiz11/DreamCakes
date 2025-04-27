f57504be86983909de485afa60c6c11270c880c3
LauraOrtiz11 <ortizlaurava@gmail.com>
2025-04-26 21:09:06 -0500

    RF002-  Rutas segun rol luego de realizar el login y vista del perfil

---
+++ DreamCakes/Controllers/ProfileController.cs
@@ -0,0 +1,41 @@
+﻿using DreamCakes.Dtos;
+using DreamCakes.Services;
+using System.Web.Mvc;
+
+namespace DreamCakes.Controllers
+{
+    [Authorize] // Requiere autenticación
+    public class ProfileController : Controller
+    {
+        private readonly UserService _userService;
+
+        public ProfileController()
+        {
+            _userService = new UserService();
+        }
+
+        public ActionResult Index()
+        {
+            // Obtener ID del usuario logueado desde la sesión
+            var userId = (int)Session["ID_Usuario"];
+            var profile = _userService.GetUserProfile(userId);
+            return View(profile);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(ProfileDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_userService.UpdateUserProfile(model))
+                {
+                    TempData["SuccessMessage"] = "Perfil actualizado correctamente";
+                    return RedirectToAction("Index");
+                }
+                ModelState.AddModelError("", "Error al actualizar el perfil");
+            }
+            return View(model);
+        }
+    }
+}
