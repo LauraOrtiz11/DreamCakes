cab1a486884a5f111c4d1fa9efe64910560dec5a
Nicolas Gonzalez <nicolasf.gonzalez1305@gmail.com>
2025-05-13 07:49:59 -0500

    RF013 - Gestión de usuarios funcional y con estilos provisionales

---
+++ DreamCakes/Services/Admin/AdminUserService.cs
@@ -0,0 +1,16 @@
+﻿using DreamCakes.Dtos.Admin;
+using DreamCakes.Repositories.Admin;
+using System.Collections.Generic;
+
+namespace DreamCakes.Services.Admin
+{
+    public class AdminUserService
+    {
+        private readonly AdminUserRepository _repository = new AdminUserRepository();
+
+        public List<AdminUserDto> GetAllUsers() => _repository.GetAllUsers();
+        public AdminUserDto GetUserById(int id) => _repository.GetUserById(id);
+        public void UpdateUser(AdminUserDto dto) => _repository.UpdateUser(dto);
+        public void DeleteUser(int id) => _repository.DeleteUser(id);
+    }
+}
